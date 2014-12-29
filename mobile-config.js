9da67190c5795741fad5425a0ff31d944b4b4fa7
Vianney Lecroart <acemtp@gmail.com>
2014-12-29 14:08:53 +0100

    first commit

---
+++ mobile-config.js
@@ -0,0 +1,12 @@
+App.info({
+//  name: 'cordova-bug', // work
+  name: 'cordova bug', // don't work
+  description: 'Cordova Bug',
+  author: 'acemtp',
+  email: 'acemtp@gmail.com',
+  website: 'http://meteor.com',
+  version: '0.0.1'
+});
+
+App.setPreference('StatusBarOverlaysWebView', 'false');
+App.setPreference('StatusBarBackgroundColor', '#000000');
